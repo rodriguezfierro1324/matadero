cba05bd741823e5fbb01777445477a6c65b396f9
lordbam2003 <bitlerarriola@gmail.com>
2017-06-26 23:16:47 -0500

    update user translate file

--- messages/es_ES/user.php
+++ messages/es_ES/user.php
@@ -2,5 +2,18 @@
 return [
 	'object'=>'Usuario',
 	'objects'=>'Usuarios',
-	'create'=>'Crear usuario'
+	'create'=>'Crear usuario',
+	'admin'=>'Crear usuario',
+//fields
+	'id' => 'ID',
+    'name' => 'Nombre',
+    'last_name' => 'Apellidos',
+    'ci' => 'CI',
+    'username' => 'username',
+    'pwd' => 'Contraseña',
+    'id_status' => 'Estado',
+    'created' => 'Fecha de creación',
+    'created_by' => 'Creado por',
+    'modified' => 'Fecha de modificación',
+    'modified_by' => 'Modificado por',
 ];?>
