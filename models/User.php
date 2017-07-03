8a9d81311754fcaf0dcd9ad7de85e68d73d7138f
lordbam2003 <bitlerarriola@gmail.com>
2017-07-03 12:20:46 -0500

    Add password user encrypt, login

--- models/User.php
+++ models/User.php
@@ -3,6 +3,9 @@
 namespace app\models;
 
 use Yii;
+use yii\web\IdentityInterface;
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
 
 /**
  * This is the model class for table "user".
@@ -19,7 +22,7 @@ use Yii;
  * @property string $modified
  * @property integer $modified_by
  */
-class User extends \yii\db\ActiveRecord
+class User extends \yii\db\ActiveRecord implements IdentityInterface
 {
     /**
      * @inheritdoc
@@ -29,13 +32,25 @@ class User extends \yii\db\ActiveRecord
         return 'user';
     }
 
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'createdAtAttribute' => 'created',
+                'updatedAtAttribute' => 'modified',
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
     /**
      * @inheritdoc
      */
     public function rules()
     {
         return [
-            [['name', 'last_name', 'ci', 'username', 'pwd', 'id_status', 'created', 'created_by', 'modified', 'modified_by'], 'required'],
+            [['name', 'last_name', 'ci', 'username', 'pwd', 'id_status'], 'required'],
             [['id_status', 'created_by', 'modified_by'], 'integer'],
             [['created', 'modified'], 'safe'],
             [['name'], 'string', 'max' => 50],
@@ -45,6 +60,53 @@ class User extends \yii\db\ActiveRecord
             [['pwd'], 'string', 'max' => 250],
             [['username'], 'unique'],
         ];
+    }
+
+    /**
+     * Finds an identity by the given ID.
+     *
+     * @param string|int $id the ID to be looked for
+     * @return IdentityInterface|null the identity object that matches the given ID.
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    /**
+     * Finds an identity by the given token.
+     *
+     * @param string $token the token to be looked for
+     * @return IdentityInterface|null the identity object that matches the given token.
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return static::findOne(['access_token' => $token]);
+    }
+
+    /**
+     * @return int|string current user ID
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string current user auth key
+     */
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+
+    /**
+     * @param string $authKey
+     * @return bool if auth key is valid for current user
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $authKey;
     }
 
     /**
@@ -65,5 +127,27 @@ class User extends \yii\db\ActiveRecord
             'modified'      => Yii::t('user', 'modified'),
             'modified_by'   => Yii::t('user', 'modified_by'),
         ];
+    }
+
+    public static function findByUsername($userName)
+    {
+        return static::findOne(['username'=>$userName]);
+    }
+
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)) {
+            if ($this->isNewRecord) {
+                $this->pwd = Yii::$app->getSecurity()->generatePasswordHash($this->pwd);
+                $this->auth_key = \Yii::$app->security->generateRandomString();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public function validatePassword($password)
+    {
+        return (Yii::$app->getSecurity()->validatePassword($password, $this->pwd));
     }
 }
